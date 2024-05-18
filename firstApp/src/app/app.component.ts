9b47338e0b38f8ae2444bbbc21456dffd6de77dc
maarten-vandun <maarten.vandun@student.kuleuven.be>
2024-05-18 22:04:59 +0200

    gekloot

--- firstApp/src/app/app.component.ts
+++ firstApp/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,41 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'firstApp';
+export class AppComponent implements OnInit{
+
+  title = 'F1Game';
+  score = 0.0;
+  multiplierSoft = 1.0;
+  multiplierLevel = 1.0;
+  time = 1000;
+
+  ngOnInit(): void {
+      console.log('AppComponent initialized');
+      this.time = 1000;
+      setInterval(() => this.incrementSoft(), this.time);
+  }
+
+  incrementSoft(): void  {
+    if (this.score > 1){
+      this.score = 0;
+    }
+    console.log(this.score);
+    if (this.score < 10){
+      this.score = parseFloat((this.score + 0.1 * this.multiplierSoft).toFixed(2));
+    }
+  }
+
+  incrementSoftmultiplier() {
+    if (this.score >= this.incrementSoftLevel()){
+      this.score -= this.incrementSoftLevel();
+      this.multiplierSoft += 1;
+      this.multiplierLevel += 1;
+    }
+  }
+
+  incrementSoftLevel() {
+    let x = this.multiplierLevel;
+    return 10 * x * x ;
+  }
 }
+
